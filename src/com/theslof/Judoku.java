ff11f8bc0fb4fbdf00bab04aa94170d8da712d90
theslof <theslof@gmail.com>
2017-08-31 19:57:12 +0200

    Cleaned up main() and implemented a benchmark. I tried 50 iterations of identical sudokus, before and after isValid optimization. Benchmark showed an 87% increase in performance.

--- src/com/theslof/Judoku.java
+++ src/com/theslof/Judoku.java
@@ -14,18 +14,38 @@ import java.util.*;
 
 
 public class Judoku {
-    private static final String[] FILE_NAME = {"simple.sud","easy.sud","intermediate.sud","expert.sud"};
+    private static final String[] FILE_NAME = {"simple.sud", "easy.sud", "intermediate.sud", "expert.sud"};
     private static final String SUDOKU_STRING = "....482...9...28.5.5.6........87...2......679.....1..4.....7...5...9..1.7..15..6.";
     private static final ArrayList<String> examples = new ArrayList<String>();
+    private static final boolean BENCHMARK = false;
+    private static final long BENCHMARK_SEED = 342212;
 
     public static void main(String[] args) {
 
         System.out.println("Judoku 0.9");
 
+        parseExampleFile(2);
+
+        if (examples.isEmpty()) {
+            System.out.println("No examples in examples list, exiting!");
+            return;
+        }
+
+        System.out.println("Read " + examples.size() + " examples from file!");
+
+        if (BENCHMARK)
+            benchmark(50, 2);
+        else
+            runRandomSudoku();
+
+    }
+
+    private static void parseExampleFile(int difficulty) {
+        examples.clear();
         FileReader file = null;
         BufferedReader buffer = null;
         try {
-            file = new FileReader(FILE_NAME[2]);
+            file = new FileReader(FILE_NAME[difficulty]);
             buffer = new BufferedReader(file);
 
             String currentLine;
@@ -45,25 +65,49 @@ public class Judoku {
                 e.printStackTrace();
             }
         }
+    }
 
-        if (examples.isEmpty()) {
-            System.out.println("No examples in examples list, exiting!");
-            return;
+    //Run through multiple sudokus and print out total and average time
+    private static void benchmark(int runs, int difficulty) {
+        //Benchmarks done with 50 runs, difficulty 2:
+        //Before isValid optimization: 50.578s, 1.01156s per puzzle
+        //After isValid optimization: 6.446s, 0.12892s per puzzle, 87% reduction
+
+        parseExampleFile(difficulty);
+
+        Random rand = new Random(BENCHMARK_SEED);
+        int size = examples.size();
+        long startTime;
+        long endTime;
+        Sudoku a;
+
+        startTime = System.currentTimeMillis();
+
+        for (int j = 0; j < runs; j++) {
+            a = new Sudoku(examples.get(rand.nextInt(size)));
+            if (!a.solve()) {
+                System.out.println("This Sudoku can't be solved!");
+            }
         }
+        endTime = System.currentTimeMillis();
+        System.out.println("Solved " + runs + " sudoku in " + (endTime - startTime) / 1000.0 +
+                "s, for an average of " + (endTime - startTime) / (1000.0 * runs) + "s per puzzle!");
+    }
 
-        System.out.println("Read " + examples.size() + " examples from file!");
-
+    private static void runRandomSudoku() {
         Random rand = new Random();
-        int i = rand.nextInt(examples.size());
+        int size = examples.size();
+        long startTime;
+        long endTime;
+        Sudoku a;
 
-        Sudoku a = new Sudoku(examples.get(i));
+        int i = rand.nextInt(size);
+
+        a = new Sudoku(examples.get(i));
 
         a.print();
         System.out.println(a);
-
-        long startTime = System.currentTimeMillis();
-        long endTime;
-
+        startTime = System.currentTimeMillis();
         if (!a.solve()) {
             System.out.println("This Sudoku can't be solved!");
         } else {
@@ -72,8 +116,5 @@ public class Judoku {
             System.out.println(a);
             System.out.println("This sudoku took " + (endTime - startTime) / 1000.0 + "s to solve!");
         }
-
-        //Import sample.sud, parse and solve
     }
-
 }
